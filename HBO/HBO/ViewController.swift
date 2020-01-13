d244f0b73d91e7f1b1398495614d882e9d63538e
Suneth <dse16137@student.nibm.lk>
2020-01-13 21:58:23 +0530

    first commit

---
+++ HBO/HBO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HBO
+//
+//  Created by Suneth on 1/13/20.
+//  Copyright © 2020 Suneth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
